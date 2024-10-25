29cb5974b95bebf22418f37a590e0f60451ca728
unknown <asolkarprutvhi7@gmail.com>
2024-10-25 15:18:59 +0530

    firstcommit

--- src/Components/Sidebar/sidebar.jsx
+++ src/Components/Sidebar/sidebar.jsx
@@ -1,34 +1,49 @@
-import React from 'react'
-import "../Sidebar/sidebar.css"
+import React, { useState } from 'react';
+import "../Sidebar/sidebar.css";
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Sidebar() {
+    const [isOpen, setIsOpen] = useState(true); // State to manage sidebar visibility
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <>
-            <div className='mainsidebar'>
+            <button className="toggleButton" onClick={toggleSidebar}>
+                ☰
+            </button>
+            <div className={`mainsidebar ${isOpen ? 'open' : 'closed'}`}>
                 
-                <div className='sidebarlogo '>
+                <div className='sidebarlogo'>
                     <img src="/Assets/logodash.png" alt="" />
                 </div>
                 <div className='line'></div>
                 <div className='navlinksidebar'>
                     <ul>
-                        
-                       
-                       
                         <li><NavLink exact to="/s" activeClassName="active">
-                        <div className='icondiv'>
-                        <img src="/Assets/menu1 (1).png" alt="" /></div></NavLink></li>
+                            <div className='icondiv'>
+                                <img src="/Assets/menu1 (1).png" alt="" />
+                            </div>
+                        </NavLink></li>
                         <li><NavLink exact to="/" activeClassName="active">
-                        <div className='icondiv'><img src="/Assets/menu2.png" alt="" /></div></NavLink></li>
+                            <div className='icondiv'>
+                                <img src="/Assets/menu2.png" alt="" />
+                            </div>
+                        </NavLink></li>
                         <li><NavLink exact to="/s" activeClassName="active">
-                        <div className='icondiv'><img src="/Assets/menu3.png" alt="" /></div></NavLink></li>
+                            <div className='icondiv'>
+                                <img src="/Assets/menu3.png" alt="" />
+                            </div>
+                        </NavLink></li>
                     </ul>
-                    
                 </div>
 
-                <div className='baseicon'><img src="/Assets/baseicon.png" alt="" /></div>
+                <div className='baseicon'>
+                    <img src="/Assets/baseicon.png" alt="" />
+                </div>
             </div>
         </>
-    )
+    );
 }
